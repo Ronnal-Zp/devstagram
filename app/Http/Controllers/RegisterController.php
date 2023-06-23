9bdff276405aa5264da6c8001e5f1a59db7be9eb
“Aldahir” <ronn.zp.3@gmail.com>
2023-06-23 02:33:05 -0500

    base inicial

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,
+        [
+            'name' => ['required', 'min:3', 'max:20'],
+            'username' => ['required', 'unique:users', 'min:3', 'max:15'],
+            'email' => ['required', 'unique:users', 'email', 'max:30'],
+            'password' => 'required'
+        ],
+        [
+            'required' => 'Este campo es requerido',
+            'unique' => 'Este correo ya existe',
+            'min' => 'Minimo de :min caracteres',
+            'max' => 'Maximo de :max caracteres',
+            'email' => 'Email no válido'
+        ]);
+    }
+
+
+
+}
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Home
+@endsection
+
+
+@section('contenido')
+    <p>Contenido del home</p>
+@endsection
